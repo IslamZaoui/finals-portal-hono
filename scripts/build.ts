ed7d8e07e53fae16bab86827561a082ad4b35dba
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-03-27 15:24:22 +0100

    setup project for production in vercel

---
+++ scripts/build.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import packageInfo from "../package.json";
+
+const external = Object.keys(packageInfo.dependencies);
+
+await Bun.$`(rm -rf dist || echo '')`;
+console.log("Starting the build process... ✨");
+
+await Bun.build({
+    entrypoints: ["src/app.ts"],
+    outdir: "dist",
+    target: "node",
+    format: "esm",
+    sourcemap: "linked",
+    minify: true,
+    external,
+});
+
+console.log("Build process finished! 🎉");
