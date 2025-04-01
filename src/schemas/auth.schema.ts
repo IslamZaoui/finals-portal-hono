a4e54c56ace08abde68a2c85d84bb5444e0eda02
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2025-04-01 17:06:41 +0100

    format

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,7 +1,7 @@
 import { sessionsTable, usersTable } from "@/db/schema";
 import { usersWithoutPasswordView } from "@/db/views";
-import { z } from "@hono/zod-openapi";
 import { schemaFactory } from "@/lib/factory";
+import { z } from "@hono/zod-openapi";
 
 const username = z
 	.string({ required_error: "Username is required" })
